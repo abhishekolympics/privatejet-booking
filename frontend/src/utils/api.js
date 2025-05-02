41b658b3012b6bb1d6bd0b5623ca946029517e6a
Abhishek Kumar Singh <50742402+abhishekolympics@users.noreply.github.com>
2025-05-02 19:08:53 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://privatejet-booking.onrender.com/api";
 
 // Create axios instance
 const api = axios.create({
